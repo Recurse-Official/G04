eae1cf70bf5a2e48475b009f2a6023b750c9577c
Darsh Manish Agrawal <138677724+DarshAgrawal14@users.noreply.github.com>
2024-12-02 15:27:16 +0530

    Update constants.py

--- backend-Django/dynamic_analysis/constants.py
+++ backend-Django/dynamic_analysis/constants.py
@@ -1,13 +1,9 @@
 # File paths
-LOGS_DIRECTORY = r"C:\Users\agraw\Documents\GitHub\G04\backend-Django\dynamic_analysis\strace_output"
-DB_PATH = r"./anlalysed_data.db"
-EMULATOR_LOCATION = r""
-EMULATOR_NAME = r"avd_name"
-LABEL = "MALWARE"   # OR "BENIGN"  
-MANUAL_LABEL  = None
+LOGS_DIRECTORY = r"path_to_log_dir"
+DB_PATH = r"path_to_db"
 SOURCE =    "directory"   
-APK_DIRECTORY = r"C:\Users\agraw\Documents\GitHub\G04\backend-Django\uploads\darsh-the-great"
-# Monkey testing settings
-MONKEY_EVENTS = 5000
-MONKEY_THROTTLE = 500
-MONKEY_DURATION = 45
+APK_DIRECTORY = r"path to apk directory"
+# USER testing settings
+USER_EVENTS = 5000
+USER_THROTTLE = 500
+USER_DURATION = 45
